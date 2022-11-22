0a8572529021923356a68ff6f13b35a15ff5a074
Bradley Showen <BShowen@me.com>
2022-11-22 18:16:21 -0500

    Display a list of genres

--- models/genre.js
+++ models/genre.js
@@ -7,7 +7,7 @@ const GenreSchema = new Schema({
 
 GenreSchema.virtual("url").get(function () {
   //We don't use an arrow function as we'll need the this object.
-  return `/genre/${this.name}`;
+  return `/genre/${this._id}`;
 });
 
 module.exports = mongoose.model("Genre", GenreSchema);
