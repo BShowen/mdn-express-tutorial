cb354813624dde23f18328db362664c6eb4154ad
Bradley Showen <BShowen@me.com>
2022-11-22 19:13:12 -0500

    Fix typo in genre route

--- models/genre.js
+++ models/genre.js
@@ -7,7 +7,7 @@ const GenreSchema = new Schema({
 
 GenreSchema.virtual("url").get(function () {
   //We don't use an arrow function as we'll need the this object.
-  return `/genre/${this._id}`;
+  return `/catalog/genre/${this._id}`;
 });
 
 module.exports = mongoose.model("Genre", GenreSchema);
